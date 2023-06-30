8eafb0a3c52d630b251bc4b97140e89052d7ddde
Saad Azam <zeroinside4u@gmail.com>
2023-06-30 15:08:43 +0500

    Add small animations for toggling between diary entry and sectio.

--- src/components/DiaryPage/DiaryEntry.jsx
+++ src/components/DiaryPage/DiaryEntry.jsx
@@ -1,26 +1,44 @@
 import { ChevronDownIcon } from "@heroicons/react/20/solid"
+import { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
 
 export default function DiaryEntry() {
+  const [isExpanded, setExpanded] = useState(true)
+
   return (
-    <div className="py-4 px-2">
+    <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="py-4 px-2"
+    >
       <div className="flex flex-row gap-2 items-center border-b border-b-lite pt-1 pb-2">
         <h5 className="font-bold text-lg">Complex Password Validation Devise</h5>
-        <ChevronDownIcon className="w-6 h-6" />
+        <motion.div animate={{ rotate: isExpanded ? 180 : 0, cursor: 'pointer' }} onClick={() => setExpanded(!isExpanded)}><ChevronDownIcon className="w-6 h-6" /></motion.div>
       </div>
-      <div className="flex flex-col gap-2.5 py-4">
-        <div className="flex flex-col gap-0.5">
-          <span><b>Type: </b><span>Bug</span></span>
-          <span><b>Updated On: </b><span>28th June, 2023</span></span>
-        </div>
-        <div>
-          <h6 className="font-semibold">Description:</h6>
-          <p>Unable to apply complex validation (like using regex) for password in devise.</p>
-        </div>
-        <div>
-          <h6 className="font-semibold">Solution: </h6>
-          <p>Devise takes in the password as instance variable. <code className="bg-zinc-700">Devise</code></p>
-        </div>
-      </div>
-    </div>
+      <AnimatePresence>
+        {isExpanded &&
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col gap-2.5 py-4"
+          >
+            <div className="flex flex-col gap-0.5">
+              <span><b>Type: </b><span>Bug</span></span>
+              <span><b>Updated On: </b><span>28th June, 2023</span></span>
+            </div>
+            <div>
+              <h6 className="font-semibold">Description:</h6>
+              <p>Unable to apply complex validation (like using regex) for password in devise.</p>
+            </div>
+            <div>
+              <h6 className="font-semibold">Solution: </h6>
+              <p>Devise takes in the password as instance variable. <code className="bg-zinc-700">Devise</code></p>
+            </div>
+          </motion.div>
+        }
+      </AnimatePresence>
+    </motion.div>
   )
 }
--- src/components/DiaryPage/Section.jsx
+++ src/components/DiaryPage/Section.jsx
@@ -1,14 +1,18 @@
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react'
 import DiaryEntry from './DiaryEntry'
+import { motion, AnimatePresence } from 'framer-motion' 
 
 export default function Section() {
+  const [isExpanded, setExpanded] = useState(false)
+
   return (
-    <section>
+    <section className="px-2 md:px-0">
       <div className="py-2.5 flex flex-row gap-2 items-center border-b-2 border-b-lite">
         <h4 className="font-serif font-bold text-2xl">Ruby on Rails</h4>
-        <ChevronDownIcon className="w-8 h-8"/>
+        <motion.div animate={{ rotate: isExpanded ? 180 : 0, cursor: 'pointer' }} onClick={() => setExpanded(!isExpanded)}><ChevronDownIcon className="w-8 h-8"/></motion.div>
       </div>
-      <DiaryEntry />
+      <AnimatePresence>{ isExpanded && <DiaryEntry /> }</AnimatePresence>
     </section>
   )
 }
