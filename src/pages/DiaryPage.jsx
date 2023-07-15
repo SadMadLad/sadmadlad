31b480cc57f112d4f3477ce498a6b69b8fb74c7a
Saad Azam <zeroinside4u@gmail.com>
2023-07-15 23:39:27 +0500

    Add bottom padding

--- src/pages/DiaryPage.jsx
+++ src/pages/DiaryPage.jsx
@@ -4,12 +4,14 @@ import Section from '../components/DiaryPage/Section'
 import Diary from '../assets/diary.json'
 
 export default function DiaryPage() {
+  const sortedDiary = Diary.sort((a, b) => a.title > b.title )
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center pb-6">
       <NavBar />
       <Hero />
       <div className="w-full md:w-3/4">
-        { Diary.map((diarySection, index) => <Section key={index} title={diarySection.title} entries={diarySection.entries}/>) }
+        {sortedDiary.map((diarySection, index) => <Section key={index} title={diarySection.title} entries={diarySection.entries} />)}
       </div>
     </div>
   )
