39094558e5a1aec157be0c746973030fbefe201e
Saad Azam <zeroinside4u@gmail.com>
2023-07-15 23:32:42 +0500

    Create a service to render jsonified code text

---
+++ services/code_snippet_jsonifier.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+class CodeSnippetJsonifier
+  attr_reader :path_to_file, :code
+
+  def initialize(path_to_file)
+    @path_to_file = path_to_file
+    @code = File.open path_to_file, 'r'
+    @code = @code.read.to_json
+  end
+
+  def call
+    File.open('output.txt', 'w') { |file| file.write(code) }
+  end
+end
+
+CodeSnippetJsonifier.new('CodeFile').call
