f73128f880544c85c72c1f35bf9f45d569150a95
Saad Azam <zeroinside4u@gmail.com>
2023-07-17 04:35:24 +0500

    Add gap to Sections

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -13,7 +13,7 @@ export default function NotesPage() {
     <div className="flex flex-col items-center pb-6">
       <NavBar />
       <SharedHero title="Notes" heroCaption="This page has some notes, tips and some resources that I learnt about on my way."/>
-      <div className="w-full md:w-3/4">
+      <div className="w-full md:w-3/4 flex flex-col gap-2.5">
         {sortedNotes.map((notesSection, index) => <Section key={index} title={notesSection.title} notes={notesSection.notes} />)}
       </div>
     </div>
