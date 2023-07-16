9b7f3f91bed8117b979810504b1b4a1d4a3ebbd1
Saad Azam <zeroinside4u@gmail.com>
2023-07-16 13:41:14 +0500

    Change theme of code snippets

--- src/components/shared/CodeSnippet.jsx
+++ src/components/shared/CodeSnippet.jsx
@@ -1,10 +1,10 @@
 // Node Imports
 import SyntaxHighlighter from "react-syntax-highlighter"
-import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { anOldHope } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 // To render highlighted code
 export default function CodeSnippet({ codeSnippet }) {
   return <div className="my-2.5"> 
-    <SyntaxHighlighter wrapLines={true} style={atomOneDarkReasonable} language={codeSnippet.language} children={codeSnippet.code}></SyntaxHighlighter>
+    <SyntaxHighlighter wrapLines={true} style={anOldHope} language={codeSnippet.language} children={codeSnippet.code}></SyntaxHighlighter>
   </div>
 }
