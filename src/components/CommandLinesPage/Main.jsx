efb5dc9f27d92ece068b62176b4387bc403965c9
Saad Azam <zeroinside4u@gmail.com>
2023-07-17 05:28:12 +0500

    Sort command lines

--- src/components/CommandLinesPage/Main.jsx
+++ src/components/CommandLinesPage/Main.jsx
@@ -6,10 +6,12 @@ import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/
 
 // List of all the Command Lines
 export default function Main() {
+  const sortedCommandLines = CommandLines.sort((a, b) => a.command > b.command)
+
   return (
     <main className="px-5">
       <ul className="list-disc flex flex-col gap-4">
-        {CommandLines.map((commandLineData, index) =>
+        {sortedCommandLines.map((commandLineData, index) =>
           <li key={index}>
             <SyntaxHighlighter wrapLines={true} style={atomOneDarkReasonable} language="vim" children={commandLineData.command}></SyntaxHighlighter>
             <p className="mt-1" dangerouslySetInnerHTML={{ __html: commandLineData.description }}></p>
