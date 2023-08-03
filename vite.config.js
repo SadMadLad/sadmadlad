b90e89faa3233ae1cff3cdd72f764c0f3cdcfc3a
Saad Azam <zeroinside4u@gmail.com>
2023-08-03 17:42:20 +0500

    Manual chunks

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ViteYaml from '@modyfi/vite-plugin-yaml'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/sadmadlad/',
-  plugins: [react(), ViteYaml()],
-  assetsInclude: ['**/*.md']
-})
+  plugins: [react()],
+  assetsInclude: ['**/*.md'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          fancy: ['framer-motion', 'react-markdown', 'rehype-raw', 'remark-gfm', 'gh-pages'],
+          syntax: ['react-syntax-highlighter'],
+        },
+      },
+    },
+  },
+});
