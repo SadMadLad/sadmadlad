6347fdf107afd460e50e18a1cb75f61f0fff51d1
Saad Azam <zeroinside4u@gmail.com>
2023-08-18 05:12:44 +0500

    Some stuff

--- src/components/shared/TransitionDiv.jsx
+++ src/components/shared/TransitionDiv.jsx
@@ -7,8 +7,8 @@ export default function TransitionDiv() {
     <motion.div
       className="fixed w-full h-full top-0 bottom-0 right-0 bg-white"
       initial={{ scaleX: 1 }}
-      animate={{ scaleX: 0, transition: { duration: 1, ease: "circOut" } }}
-      exit={{ scaleX: 1, transition: { duration: 1, ease: "circIn" } }}
+      animate={{ scaleX: 0, transition: { duration: 0.33, ease: "circOut" } }}
+      exit={{ scaleX: 1, transition: { duration: 0.33, ease: "circIn" } }}
       style={{ originX: isPresent ? 0 : 1 }}
     />
   )
