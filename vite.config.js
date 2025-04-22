bf2edcedbc043917dedc54b0d0664aa1399d4c32
Saad Azam <zeroinside4u@gmail.com>
2025-04-23 04:58:50 +0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sadmadlad",
   plugins: [vue(), svgLoader({ defaultImport: "component" }), ViteYaml()],
   resolve: {
     alias: {
