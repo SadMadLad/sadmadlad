0d0e884a2059b9854a15ebcec90ca4b0c1085255
Saad Azam <zeroinside4u@gmail.com>
2025-04-23 05:04:59 +0500

    Updates

---
+++ assets/test_note-akQSD1CT.js
@@ -0,0 +1,9 @@
+const e=`# **This is a Test Note**
+
+This is my **first** note. Let's see how it *looks* on the page. Testing if markdown works fine.
+<p class="font-black">Hey! Raw HTML with Tailwind works too!</p>
+
+Here is a cool image of \`Sub-Zero\`.
+
+<img src="/images/Sub-Zero.png" alt="Sub-Zero">
+`;export{e as default};
