42924cd7becc3b2030516f1589eb139ee24dedb8
Saad Azam <zeroinside4u@gmail.com>
2025-04-23 05:27:54 +0500

    Updates

--- assets/test_note-D9MHMp5W.js
+++ assets/test_note-D9MHMp5W.js
@@ -5,5 +5,5 @@ This is my **first** note. Let's see how it *looks* on the page. Testing if mark
 
 Here is a cool image of \`Sub-Zero\`.
 
-<img src="/images/Sub-Zero.png" alt="Sub-Zero">
+<img src="/sadmadlad/images/Sub-Zero.png" alt="Sub-Zero">
 `;export{e as default};
