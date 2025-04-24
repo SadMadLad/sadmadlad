1991cbdd184e8c64a3be68049de95c6637cf082d
Saad Azam <zeroinside4u@gmail.com>
2025-04-24 23:58:52 +0500

    Updates

--- assets/test_note-CXs2-fjI.js
+++ assets/test_note-CXs2-fjI.js
@@ -1,21 +1,27 @@
 const n=`# **This is a Test Note**
+
 ### A subheading
+
 This is my **first** note. Let's see how it _looks_ on the page. Testing if markdown works fine.
+
 <p class="font-black">Hey! Raw HTML with Tailwind works too!</p>
 
 Here is a cool image of \`Sub-Zero\`.
 <img src="/sadmadlad/images/Sub-Zero.png" alt="Sub-Zero">
 Do the lists work?
-* Like this
-* and this
-* and this
+
+- Like this
+- and this
+- and this
 
 \`\`\`javascript
-const str = 'asdasd';
+const str = "asdasd";
 \`\`\`
+
 \`\`\`ruby
 str = "for different languages?"
 \`\`\`
+
 \`\`\`cpp
 int x = 1000;
 int x[2] = {1, 2};
