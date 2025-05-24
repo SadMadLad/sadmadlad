8bd14f1c3077af3d6d02bdd8c7bf3dd93df4d5a5
Saad Azam <zeroinside4u@gmail.com>
2025-05-24 15:13:04 +0500

    Attempt for links

--- vite.config.js
+++ vite.config.js
@@ -17,11 +17,11 @@ export default defineConfig({
 		port: 5174,
 	},
 	build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        fallback: 'index.html',
-      }
-    }
-  }
+		rollupOptions: {
+			input: {
+				main: "index.html",
+				fallback: "index.html",
+			},
+		},
+	},
 });
